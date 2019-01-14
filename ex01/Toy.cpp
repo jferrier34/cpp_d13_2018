52ec0c8efba76e0ac26de41c4146e6ee3cfeb47a
Julien Ferrier <jferrier@localhost.localdomain>
2019-01-14 15:48:43 +0100

    [modify] delete the main

--- ex01/Toy.cpp
+++ ex01/Toy.cpp
@@ -59,17 +59,3 @@ void Toy::setName(const std::string &name)
 	_name = name;
 }
 
-
-int  main()
-{
-Toy  toto;
-Toy ET(Toy::ALIEN , "green", "./alien.txt");
-toto.setName("TOTO !");
-if (toto.getType () == Toy:: BASIC_TOY)
-std::cout  << "basic  toy: " << toto.getName () << std::endl
-<< toto.getAscii () << std::endl;
-if (ET.getType () == Toy:: ALIEN)
-std::cout  << "this  alien is: " << ET.getName () << std::endl
-<< ET.getAscii () << std::endl;
-return  0;
-}
